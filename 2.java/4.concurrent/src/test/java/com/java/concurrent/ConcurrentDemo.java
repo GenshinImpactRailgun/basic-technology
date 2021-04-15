5a82e7b6b28764b8512c7a5cbd8965d540531011
only my railgun <2354547500@qq.com>
2021-04-15 17:45:05 +0800

    并发模块提交更新；

---
+++ 2.java/4.concurrent/src/test/java/com/java/concurrent/ConcurrentDemo.java
@@ -0,0 +1,17 @@
+package com.java.concurrent;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @Author: railgun
+ * 2021/4/15 17:36
+ * PS:java 并发编程
+ */
+public class ConcurrentDemo {
+
+    @Test
+    public void test1(){
+        System.out.println("开启并发编程");
+    }
+
+}
