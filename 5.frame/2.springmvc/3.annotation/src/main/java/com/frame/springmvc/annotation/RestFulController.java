84d3dfbd3506908a2bc85eb0ee1015cee7dac5c3
only my railgun <2354547500@qq.com>
2021-06-16 23:15:31 +0800

    RestFul 风格 更新；

---
+++ 5.frame/2.springmvc/3.annotation/src/main/java/com/frame/springmvc/annotation/RestFulController.java
@@ -0,0 +1,50 @@
+package com.frame.springmvc.annotation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @Author: railgun
+ * 2021/6/16 22:20
+ * PS:
+ **/
+@Controller
+public class RestFulController {
+
+    /**
+     * railgun
+     * 2021/6/16 23:04
+     * PS: 普通方式请求
+     **/
+    @RequestMapping("test")
+    public String test(int a, int b, Model model) {
+        model.addAttribute("msg", a + b);
+        return "hello";
+    }
+
+    /**
+     * railgun
+     * 2021/6/16 23:04
+     * PS: RestFul 风格
+     **/
+    @RequestMapping("test2/{a}/{b}")
+    public String test2(@PathVariable int a, @PathVariable int b, Model model) {
+        model.addAttribute("msg", a + b);
+        return "hello";
+    }
+
+    /**
+     * railgun
+     * 2021/6/16 23:04
+     * PS: 指定请求方式
+     **/
+    @RequestMapping(value = "test3/{a}/{b}", method = RequestMethod.POST)
+    public String test3(@PathVariable int a, @PathVariable int b, Model model) {
+        model.addAttribute("msg", a + b);
+        return "hello";
+    }
+
+}
