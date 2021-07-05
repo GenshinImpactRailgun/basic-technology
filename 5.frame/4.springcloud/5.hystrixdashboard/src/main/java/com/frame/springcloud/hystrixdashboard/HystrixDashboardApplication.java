cdce1e4f8de883cb015da6b57414ff4dbced551e
only my railgun <2354547500@qq.com>
2021-07-05 13:32:10 +0800

    Hystrix Dashboard 流监控； update

---
+++ 5.frame/4.springcloud/5.hystrixdashboard/src/main/java/com/frame/springcloud/hystrixdashboard/HystrixDashboardApplication.java
@@ -0,0 +1,18 @@
+package com.frame.springcloud.hystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @Author: railgun
+ * 2021/7/5 12:39
+ * PS: 主启动类
+ **/
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class);
+    }
+}
